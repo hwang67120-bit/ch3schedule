50b819341db9bb10f8cbdab73a627a461e7d402a
hwang67120 <hwang67120@gmail.com>
2026-04-21 19:52:42 +0900

    BCrypt 추가

---
+++ src/main/java/com/nodeajva/ch3schedule/config/PasswoedEncoder.java
@@ -0,0 +1,19 @@
+package com.nodeajva.ch3schedule.config;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PasswoedEncoder {
+
+    public String encode(String rawPassword){
+        return BCrypt.withDefaults()
+                .hashToString(BCrypt.MIN_COST, rawPassword.toCharArray());
+    }
+
+    public boolean matches(String rawPassword, String encodedPassword) {
+        BCrypt.Result result = BCrypt.verifyer()
+                .verify(rawPassword.toCharArray(), encodedPassword);
+        return result.verified;
+    }
+}
