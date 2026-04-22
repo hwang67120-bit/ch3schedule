e848ee77034897d9dc527c5fb2641570883cbcaf
hwang67120 <hwang67120@gmail.com>
2026-04-22 16:11:09 +0900

    default Schedule 추가

--- src/main/java/com/nodeajva/ch3schedule/repository/ScheduleRepository.java
+++ src/main/java/com/nodeajva/ch3schedule/repository/ScheduleRepository.java
@@ -1,9 +1,8 @@
 package com.nodeajva.ch3schedule.repository;
 
 import com.nodeajva.ch3schedule.entity.Schedule;
-
+import com.nodeajva.ch3schedule.exception.ScheduleNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
 
@@ -16,4 +15,8 @@ public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     List<Schedule> findAllByOrderByCreatedAtDesc();
 
     List<Schedule> findByUserId(Long userid);
+
+    default Schedule getById(Long id) {
+        return findById(id).orElseThrow(ScheduleNotFoundException::new);
+    }
 }
