8079fa55f981c2aa66193c583233e5a6ed23180e
hwang67120 <hwang67120@gmail.com>
2026-04-22 19:36:35 +0900

    refactor: 주석 추가

--- src/main/java/com/nodeajva/ch3schedule/repository/CommentRepository.java
+++ src/main/java/com/nodeajva/ch3schedule/repository/CommentRepository.java
@@ -1,18 +1,35 @@
 package com.nodeajva.ch3schedule.repository;
 
 import com.nodeajva.ch3schedule.entity.Comment;
-import com.nodeajva.ch3schedule.entity.Schedule;
 import com.nodeajva.ch3schedule.exception.CommentNotFoundException;
-import com.nodeajva.ch3schedule.exception.ScheduleNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    /**
+     * 일정 ID로 댓글 목록 조회
+     *
+     * @param scheduleId 일정 ID
+     * @return 해당 일정의 댓글 목록
+     */
     List<Comment> findByScheduleId(Long scheduleId);
 
+    /**
+     * 일정 ID로 댓글 삭제
+     *
+     * @param scheduleId 일정 ID
+     */
     void deleteByScheduleId(Long scheduleId);
 
+    /**
+     * ID로 댓글 조회
+     *
+     * @param id 댓글 ID
+     * @return 댓글 엔티티
+     * @throws CommentNotFoundException 댓글을 찾을 수 없는 경우
+     */
     default Comment load(Long id) {
         return findById(id).orElseThrow(CommentNotFoundException::new);
     }
--- src/main/java/com/nodeajva/ch3schedule/repository/UserRepository.java
+++ src/main/java/com/nodeajva/ch3schedule/repository/UserRepository.java
@@ -2,7 +2,6 @@ package com.nodeajva.ch3schedule.repository;
 
 import com.nodeajva.ch3schedule.entity.User;
 import com.nodeajva.ch3schedule.exception.InvalidPasswordException;
-
 import com.nodeajva.ch3schedule.exception.UserNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -10,13 +9,32 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-
+    /**
+     * 로그인 ID로 사용자 조회
+     *
+     * @param loginId 로그인 ID
+     * @return Optional로 감싼 사용자 엔티티
+     */
     Optional<User> findByLoginId(String loginId);
 
+    /**
+     * 로그인 ID로 사용자 조회 (예외 발생)
+     *
+     * @param loginId 로그인 ID
+     * @return 사용자 엔티티
+     * @throws InvalidPasswordException 사용자를 찾을 수 없는 경우
+     */
     default User loadByLoginId(String loginId) {
         return findByLoginId(loginId).orElseThrow(InvalidPasswordException::new);
     }
 
+    /**
+     * ID로 사용자 조회
+     *
+     * @param id 사용자 ID
+     * @return 사용자 엔티티
+     * @throws UserNotFoundException 사용자를 찾을 수 없는 경우
+     */
     default User load(Long id) {
         return findById(id).orElseThrow(UserNotFoundException::new);
     }
