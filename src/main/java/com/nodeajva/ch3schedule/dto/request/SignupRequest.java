424bac0629e615990cd779928f97bb6f539ab4ab
hwang67120 <hwang67120@gmail.com>
2026-04-22 09:15:38 +0900

    record로 변경

--- src/main/java/com/nodeajva/ch3schedule/dto/request/SignupRequest.java
+++ src/main/java/com/nodeajva/ch3schedule/dto/request/SignupRequest.java
@@ -3,7 +3,6 @@ package com.nodeajva.ch3schedule.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record SignupRequest(
