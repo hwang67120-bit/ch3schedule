815bd31a414d6ffebd7e58d66aaff54d90d6986b
hwang67120 <hwang67120@gmail.com>
2026-04-22 17:54:52 +0900

    refactor: Query 추가

--- src/main/java/com/nodeajva/ch3schedule/repository/ScheduleRepository.java
+++ src/main/java/com/nodeajva/ch3schedule/repository/ScheduleRepository.java
@@ -3,6 +3,8 @@ package com.nodeajva.ch3schedule.repository;
 import com.nodeajva.ch3schedule.entity.Schedule;
 import com.nodeajva.ch3schedule.exception.ScheduleNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.List;
 
 
@@ -19,4 +21,7 @@ public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     default Schedule load(Long id) {
         return findById(id).orElseThrow(ScheduleNotFoundException::new);
     }
+
+    @Query("SELECT s FROM Schedule s ORDER BY s.createdAt  DESC")
+    List<Schedule> findRecent();
 }
