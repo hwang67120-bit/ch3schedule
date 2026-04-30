82f2c305491609701d7ad119a849ea3776014c28
HwangSunNam <hwang67120@gmail.com>
2026-04-30 17:57:13 +0900

    전체 수정

--- src/main/java/com/nodeajva/ch3schedule/entity/Schedule.java
+++ src/main/java/com/nodeajva/ch3schedule/entity/Schedule.java
@@ -1,40 +1,46 @@
 package com.nodeajva.ch3schedule.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "schedules")
+@Table(name = "schedule")
 @Getter
 @NoArgsConstructor
 public class Schedule extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @Column(nullable = false)
-    private String title;
+	@Column(nullable = false)
+	private String title;
 
-    private String content;
+	private String content;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
 
+	public Schedule(String title, String content, User user) {
+		this.title = title;
+		this.content = content;
+		this.user = user;
 
-    public Schedule(String title, String content, User user) {
-        this.title = title;
-        this.content = content;
-        this.user = user;
+	}
 
-    }
+	public void update(String title, String content) {
+		this.title = title;
+		this.content = content;
 
-
-    public void update(String title, String content) {
-        this.title = title;
-        this.content = content;
-
-    }
+	}
 }
--- src/main/java/com/nodeajva/ch3schedule/entity/User.java
+++ src/main/java/com/nodeajva/ch3schedule/entity/User.java
@@ -1,39 +1,42 @@
 package com.nodeajva.ch3schedule.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-
 @Entity
-@Table(name = "users")
+@Table(name = "user")
 @Getter
 @NoArgsConstructor
 public class User extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @Column(unique = true, nullable = false)
-    private String loginId;
+	@Column(unique = true, nullable = false)
+	private String loginId;
 
-    @Column(nullable = false)
-    private String password;
+	@Column(nullable = false)
+	private String password;
 
-    @Column(nullable = false)
-    private String userName;
+	@Column(nullable = false)
+	private String userName;
 
-    @Column(nullable = false)
-    private String email;
+	@Column(nullable = false)
+	private String email;
 
+	// 생성자
+	public User(String loginId, String password, String userName, String email) {
+		this.loginId = loginId;
+		this.password = password;
+		this.userName = userName;
+		this.email = email;
 
-    // 생성자
-    public User(String loginId, String password, String userName, String email) {
-        this.loginId = loginId;
-        this.password = password;
-        this.userName = userName;
-        this.email = email;
-
-    }
+	}
 }
